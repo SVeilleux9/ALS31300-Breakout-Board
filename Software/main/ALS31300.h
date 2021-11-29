52157f03f638da68ee312196e655d61d54d64058
SVeilleux9 <SVeilleux9@gmail.com>
2021-11-29 13:25:13 -0500

    Updated Code
    Pushing the latest version of the code/library.

--- Software/main/ALS31300.h
+++ Software/main/ALS31300.h
@@ -1,161 +1,164 @@
+#include "Arduino.h"
+#include <Wire.h> 
+
 struct Reg{
     const uint8_t 
         address,
         numBits,
-        offset;
-    
+        offset; 
 };
 
-struct Field{
-    uint16_t x, y, z;
+struct FullRMA{
+    int16_t x, y, z, temperature;
 };
 
 namespace RegMap{
 
-    uint8_t addresses[8]{0x02, 0x03, 0x0D, 0x0E, 0x0F, 0x27, 0x28, 0x29};    
-
     // Address, Length, Offset
     constexpr Reg
         // 0x02 address
-        BW_Select           {RegMap::addresses[0], 3, 21},
-        Hall_Mode           {RegMap::addresses[0], 2, 19},
-        I2C_CRC_Enable      {RegMap::addresses[0], 1, 18},
-        Disable_Slave_ADC   {RegMap::addresses[0], 1, 17},
-        Slave_Address       {RegMap::addresses[0], 7, 10},
-        I2C_Threshold       {RegMap::addresses[0], 1,  9},
-        Channel_Z_Enable    {RegMap::addresses[0], 1,  8},
-        Channel_Y_Enable    {RegMap::addresses[0], 1,  7},
-        Channel_X_Enable    {RegMap::addresses[0], 1,  6},
-        NINT_Latch_Enable   {RegMap::addresses[0], 1,  5},
-        Customer_EEPROM1    {RegMap::addresses[0], 1,  0},
+        BW_Select           {0x02, 3, 21},
+        Hall_Mode           {0x02, 2, 19},
+        I2C_CRC_Enable      {0x02, 1, 18},
+        Disable_Slave_ADC   {0x02, 1, 17},
+        Slave_Address       {0x02, 7, 10},
+        I2C_Threshold       {0x02, 1,  9},
+        Channel_Z_Enable    {0x02, 1,  8},
+        Channel_Y_Enable    {0x02, 1,  7},
+        Channel_X_Enable    {0x02, 1,  6},
+        NINT_Latch_Enable   {0x02, 1,  5},
+        Customer_EEPROM1    {0x02, 1,  0},
 
         // 0x03 address
-        Signed_NINT_Enable  {RegMap::addresses[1], 1, 24},
-        NINT_Mode           {RegMap::addresses[1], 1, 23},
-        NINT_EEPROM         {RegMap::addresses[1], 1, 22},
-        NINT_EEPROM_Status  {RegMap::addresses[1], 1, 21},
-        NINT_EEPROM_Enable  {RegMap::addresses[1], 1, 20},
-        Z_NINT_Enable       {RegMap::addresses[1], 1, 19},
-        Y_NINT_Enable       {RegMap::addresses[1], 1, 18},
-        X_NINT_Enable       {RegMap::addresses[1], 1, 17},
-        Z_NINT_Threshold    {RegMap::addresses[1], 6, 12},
-        Y_NINT_Threshold    {RegMap::addresses[1], 6,  6},
-        X_NINT_Threshold    {RegMap::addresses[1], 6,  0},
+        Signed_NINT_Enable  {0x03, 1, 24},
+        NINT_Mode           {0x03, 1, 23},
+        NINT_EEPROM         {0x03, 1, 22},
+        NINT_EEPROM_Status  {0x03, 1, 21},
+        NINT_EEPROM_Enable  {0x03, 1, 20},
+        Z_NINT_Enable       {0x03, 1, 19},
+        Y_NINT_Enable       {0x03, 1, 18},
+        X_NINT_Enable       {0x03, 1, 17},
+        Z_NINT_Threshold    {0x03, 6, 12},
+        Y_NINT_Threshold    {0x03, 6,  6},
+        X_NINT_Threshold    {0x03, 6,  0},
 
         // 0x0D address
-        Customer_EEPROM2    {RegMap::addresses[2], 26, 0},
+        Customer_EEPROM2    {0x0D, 26, 0},
 
         // 0x0E address
-        Customer_EEPROM3    {RegMap::addresses[3], 26, 0},
+        Customer_EEPROM3    {0x0E, 26, 0},
         
         // 0x0F address
-        Customer_EEPROM4    {RegMap::addresses[4], 26, 0},
+        Customer_EEPROM4    {0x0F, 26, 0},
 
         // 0x27 address
-        Low_Power_Mode_Count_Max {RegMap::addresses[5], 1, 23},
-        I2C_Loop_Mode       {RegMap::addresses[5], 1, 22},
-        Sleep               {RegMap::addresses[5], 1, 21},
+        Low_Power_Mode_Count_Max {0x27, 1, 23},
+        I2C_Loop_Mode       {0x27, 1, 22},
+        Sleep               {0x27, 1, 21},
 
         // 0x28 address
-        X_Axis_MSB          {RegMap::addresses[6], 8, 24},
-        Y_Axis_MSB          {RegMap::addresses[6], 8, 16},
-        Z_Axis_MSB          {RegMap::addresses[6], 8, 8},
-        New_Data            {RegMap::addresses[6], 1, 7},
-        Interrupt           {RegMap::addresses[6], 1, 6},
-        Temperature_MSB     {RegMap::addresses[6], 6, 0},
+        X_Axis_MSB          {0x28, 8, 24},
+        Y_Axis_MSB          {0x28, 8, 16},
+        Z_Axis_MSB          {0x28, 8, 8},
+        New_Data            {0x28, 1, 7},
+        Interrupt           {0x28, 1, 6},
+        Temperature_MSB     {0x28, 6, 0},
 
         // 0x28 address
-        Interrupt_Write     {RegMap::addresses[7], 1, 20},
-        X_Axis_LSB          {RegMap::addresses[7], 4, 16},
-        Y_Axis_LSB          {RegMap::addresses[7], 4, 12},
-        Z_Axis_LSB          {RegMap::addresses[7], 4, 8},
-        Hall_Mode_Status    {RegMap::addresses[7], 2, 6},
-        Temperature_LSB     {RegMap::addresses[7], 6, 0};
+        Interrupt_Write     {0x29, 1, 20},
+        X_Axis_LSB          {0x29, 4, 16},
+        Y_Axis_LSB          {0x29, 4, 12},
+        Z_Axis_LSB          {0x29, 4, 8},
+        Hall_Mode_Status    {0x29, 2, 6},
+        Temperature_LSB     {0x29, 6, 0};
     
-}
-
-uint32_t i2c_read(const uint16_t address);
-void     i2c_write(const uint16_t address, const uint32_t val);
-
-template <class X, class V>
-constexpr auto find(X& x, V key) {
-    std::size_t i = 0;
-    while(i < x.size()) {
-        if(x[i] == key) return i;
-        ++i;
-    }
-    return i;
 }
 
 class ALS31300{
 private: 
     uint8_t m_deviceAddress;
 
-    std::array<uint32_t, 8> RegMap{0x00000000, 0x00000000, 0x00000000, 0x00000000,
-                                   0x00000000, 0x00000000, 0x87654321, 0x87654321};
-
     constexpr uint32_t getMask(uint8_t numBits, uint8_t offset){
-        return ((1 << numBits) - 1) << offset;
+        return (((uint32_t)1 << numBits) - 1) << offset;
     }
 
-    template <typename T, size_t size>
-    constexpr T splitRegDecode(std::array<Reg,size> regs){
-        size_t runningOffset = 0;
-        T output = 0;
-        for(size_t i=0; i<size; i++){
-            output += static_cast<T>(getValue(regs[i]) << runningOffset);
-            runningOffset += regs[i].numBits;
-        }
-        return output;
+    constexpr uint16_t splitRegDecode(Reg regLow, uint32_t lsb, Reg regHigh, uint32_t msb){
+        return static_cast<uint16_t>((getValue(regHigh, msb) << regLow.numBits) + getValue(regLow, lsb));
+    }
+
+    constexpr int16_t signExtend(uint32_t data, int width){
+        int16_t mask = 1L << (width - 1);
+        data = data & ((1 << width) - 1);
+        return static_cast<int16_t>((data ^ mask) - mask);
     }
 
 public: 
-    ALS31300(uint8_t deviceAddress) : m_deviceAddress(deviceAddress){
-    };
+    ALS31300(uint8_t deviceAddress) : m_deviceAddress(deviceAddress){};
 
-    void read(uint32_t address){
-        auto loc = find(RegMap::addresses, address);
-        if(loc != RegMag.addresses.size()){
-            RegMap[loc] = i2c_read(address));
-        }
+    uint32_t read(uint8_t address){
+        Wire.beginTransmission(m_deviceAddress);
+        Wire.write(address);
+        Wire.endTransmission(false);
+
+        Wire.requestFrom(static_cast<int>(m_deviceAddress), 4);
+        uint32_t value = static_cast<uint32_t>(Wire.read() & 0xFF) << 24;
+        value += static_cast<uint32_t>(Wire.read() & 0xFF) << 16;
+        value += static_cast<uint32_t>(Wire.read() & 0xFF) << 8;
+        value += static_cast<uint32_t>(Wire.read() & 0xFF);
+        delay(10);
+
+        return value;
+    }
+
+    uint32_t read(Reg reg){
+        return read(reg.address);
     }
     
     void write(uint8_t address, uint32_t value){
-        i2c_write(RegMap::addresses[i], RegMap[i]);
+        Wire.beginTransmission(m_deviceAddress);
+        Wire.write(address);
+        Wire.write(static_cast<uint8_t>(value >> 24));
+        Wire.write(static_cast<uint8_t>(value >> 16));
+        Wire.write(static_cast<uint8_t>(value >> 8));
+        Wire.write(static_cast<uint8_t>(value));    
+        Wire.endTransmission();
+        delay(100);
+    }
+
+    void write(Reg reg, uint32_t value){
+        write(reg.address, value);
     }
 
     void unlock(){
         write(0x35, 0x2C413534);
     }
 
-    constexpr uint32_t getValue(Reg reg){
-        auto loc = find(RegMap::addresses, reg.address);
-        if(loc != RegMag.addresses.size()){
-            auto mask = getMask(reg.numBits, reg.offset);
-            return (RegMap[loc] & mask) >> reg.offset;
-        }
-        return 0;
+    constexpr uint32_t getValue(Reg reg, uint32_t value){
+        auto mask = getMask(reg.numBits, reg.offset);
+        return (value & mask) >> reg.offset;
     }
 
-    constexpr void setValue(Reg reg, uint32_t value){
-        auto loc = find(RegMap::addresses, reg.address);
-        if(loc != RegMag.addresses.size()){
-            auto mask = getMask(reg.numBits, reg.offset);
-            RegMap[loc] &= !mask; // clear bits
-            RegMap[loc] |= value;
-        }
+    constexpr void setValue(Reg reg, uint32_t &regValue, uint32_t value){
+        auto mask = getMask(reg.numBits, reg.offset);
+        regValue &= ~mask; // clear bits
+        regValue |= value << reg.offset;
     }
 
-    constexpr Field getField(){
-        return Field{
-            splitRegDecode<uint16_t,2>(std::array<Reg,2>{RegMap::X_Axis_LSB, RegMap::X_Axis_MSB}),
-            splitRegDecode<uint16_t,2>(std::array<Reg,2>{RegMap::Y_Axis_LSB, RegMap::Y_Axis_MSB}),
-            splitRegDecode<uint16_t,2>(std::array<Reg,2>{RegMap::Z_Axis_LSB, RegMap::Z_Axis_MSB})
+    FullRMA readFull(){
+        uint32_t lsb = read(RegMap::X_Axis_LSB.address);
+        uint32_t msb = read(RegMap::X_Axis_MSB.address);
+
+        auto X = splitRegDecode(RegMap::X_Axis_LSB, lsb, RegMap::X_Axis_MSB, msb);
+        auto Y = splitRegDecode(RegMap::Y_Axis_LSB, lsb, RegMap::Y_Axis_MSB, msb);
+        auto Z = splitRegDecode(RegMap::Z_Axis_LSB, lsb, RegMap::Z_Axis_MSB, msb);
+        auto T = splitRegDecode(RegMap::Temperature_LSB, lsb, RegMap::Temperature_MSB, msb);
+
+        return FullRMA{
+            signExtend(X, RegMap::X_Axis_LSB.numBits + RegMap::X_Axis_MSB.numBits),
+            signExtend(Y, RegMap::Y_Axis_LSB.numBits + RegMap::Y_Axis_MSB.numBits),
+            signExtend(Z, RegMap::Z_Axis_LSB.numBits + RegMap::Z_Axis_MSB.numBits),
+            signExtend(T, RegMap::Temperature_LSB.numBits + RegMap::Temperature_MSB.numBits)
         };
-    }
-
-    constexpr uint16_t getTemperature(){
-        return splitRegDecode<uint16_t,2>(std::array<Reg,2>{RegMap::Temperature_LSB, RegMap::Temperature_MSB});
     }
 };
 
